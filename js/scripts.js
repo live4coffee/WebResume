7cd11051c5e22cf2031784118579958176613577
live4coffee <jfriendrst@gmail.com>
2021-09-12 14:53:41 -0500

    user counter

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,14 @@ $(function() {
 	});
 });
 
+  $('.count-numbers').each(function () {
+      $(this).prop('Counter', 0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 5000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
